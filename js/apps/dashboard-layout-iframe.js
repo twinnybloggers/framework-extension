815859047ed8ef92272d620bf031498b9aa207a1
Twinny Support <21377149+twinnybloggers@users.noreply.github.com>
2022-03-20 16:03:59 +0700

    Delete js directory

--- js/apps/dashboard-layout-iframe.js
+++
@@ -1,7 +0,0 @@
-$('.section .widget').each(function () {
-    var widget_id = $(this).attr('id');
-    if (typeof(widget_id) == 'undefined') {
-        return;
-    }
-    $(this).find('.widget-wrap3 .widget-content').append(html_tag('span', '.twinny-widget-id', widget_id));
-});
--- js/apps/post-speed.js
+++
@@ -1,7 +0,0 @@
-/* 
-  * Untuk mengubah tajuk lisensi ini, pilih Tajuk Lisensi di Properti Proyek.
-  * Untuk mengubah file template ini, pilih Alat | Template
-  * dan buka template di editor.
- */
-
-
